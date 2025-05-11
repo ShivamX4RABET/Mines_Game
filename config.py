40c82909c8531cfc9e1980b04af9e07dd5298324
ShivamX4RABET <shivamguptarooting@gmail.com>
2025-05-11 06:10:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ TOKEN = os.getenv('TOKEN')
 
 # List of admin user IDs
 ADMINS = [6129189597]  # Replace with your Telegram user ID
-GROUP_CHAT_ID = -1002315817553, -1002575094500  # Replace with your group chat ID
